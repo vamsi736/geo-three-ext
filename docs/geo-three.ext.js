4a7105b406d1528684c46d7895b65805e4aaa552
vamsi736 <52200196+vamsi736@users.noreply.github.com>
2025-07-17 10:05:36 +0300

    Update geo-three.ext.js
    updated qatar cordinates

--- docs/geo-three.ext.js
+++ docs/geo-three.ext.js
@@ -9,7 +9,8 @@ class GeoThreeExtension extends Autodesk.Viewing.Extension {
 	var provider = new Geo.MapBoxProvider(MAPBOX_TOKEN, MAPBOX_STYLE, Geo.MapBoxProvider.STYLE);
 
         var map = new Geo.MapView(Geo.MapView.PLANAR, provider);
-        map.position.set(14900,-27300,-45);
+        const coords = Geo.UnitsUtils.datumsToSpherical(25.276987, 51.520008); // Coordinates for Doha
+	map.position.set(coords.x, 0, -coords.y);
         viewer.overlays.addScene('map');
         viewer.overlays.addMesh(map, 'map');
         map.updateMatrixWorld(false);
